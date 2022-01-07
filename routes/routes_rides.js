6aa8adaaad647b2ad38305490de80a4980b29647
lterates <9180199@esmad.ipp.pt>
2022-01-07 02:08:01 +0000

    Routing for rides with GET POST & DELETE requests

---
+++ routes/routes_rides.js
@@ -0,0 +1,38 @@
+const express = require('express'); 
+const router = express.Router(); 
+const { body, validationResult } = require('express-validator'); 
+const controller = require('../controllers/controller_rides');
+
+router.route('/').get(function (req, res) {
+    if(req.body.email && req.body.email != '') {
+       controller.listByEmail(req, res);
+       console.log("GET RIDES BY EMAIL")
+    } else if (req.body.id && req.body.id != '') {
+        controller.findById(req, res);
+        console.log("GET RIDES BY ID")
+    } else {
+       controller.listAll(req, res); 
+       console.log("GET ALL RIDES")
+    }
+}).post([
+    body('price').notEmpty().isNumeric(),
+    body('start_point').notEmpty().escape(),
+    body('end_point').notEmpty().escape(),
+    body('start_time').notEmpty().isNumeric(),
+    body('start_day').optional(),
+    body('ride_status').notEmpty().escape(),
+],function(req, res) { 
+    const errors = validationResult(req); 
+    if(errors.isEmpty()) {
+       controller.create(req, res); 
+    } else {
+        res.status(400).send(errors); 
+    }
+}).delete(function(req, res) { 
+    if(req.body.id && req.body.id != '') {
+        controller.deleteUser(req, res);
+        console.log("DELETED USER")
+    }
+})
+
+module.exports = router; 
