e5b4b2ce1c8321357cda040e0f411bdc5a0fe7d1
Jorge Daniel <jorge.daniel11@outlook.com>
2022-01-19 14:47:37 +0000

    Controller FavPlaces

--- controllers/controller_fav_places.js
+++ controllers/controller_fav_places.js
@@ -5,15 +5,43 @@ const FavPlaces = modelFavPlaces.FavPlaces;
 
 //ADD NEW FAVORITE PLACE -- POST
 const addNewPlace = (req, res) => {
-
+    FavPlaces.create({
+        place_name: req.body.place_name,
+        place_cords: req.body.place_cords
+    }).then((result) => {
+        res.status(200).json(result);
+    }).catch((error) => {
+        res.status(400).send('Error: ' + error);
+    })
 }
 
 //EDIT FAAVORITE PLACE -- PUT
 const editPlace = (req, res) => {
-
+    FavPlaces.update({
+        place_name: req.body.place_name,
+        where: {
+            id: req.params.id
+        }
+    }).then((result) => {
+        res.status(200).json(result)
+    }).catch((error) => {
+        res.status(400).send('Error: ' + error);
+    });
 }
 
 //DELETE FAVORITE PLACE -- DELETE
 const deletePlace = (req, res) => {
-    
+    FavPlaces.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then((result) => {
+        res.status(200).json(result);
+    }).catch((error) => {
+        res.status(400).send('Error: ' + error);
+    })
 }
+
+exports.addNewPlace = addNewPlace;
+exports.editPlace = editPlace;
+exports.deletePlace = deletePlace;
